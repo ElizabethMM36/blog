7dbef7911831e3ab9b10f9a8192e85f999ddef6f
ElizabethMM36 <194572580+ElizabethMM36@users.noreply.github.com>
2025-06-05 15:03:22 +0400

    Ex 4.8

---
+++ bloglist/tests/blog_api.test.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog =require('../models/blog')
+const api = supertest(app)
+const initialBlogs = [{title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7},
+    {    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5},
+    {title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12},
+    {  title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10}
+]
+beforeEach(async () =>{
+    await Blog.deleteMany({})
+    await Blog.insertMany(initialBlogs)
+
+
+})
+
+test('blogs are returned as JSON', async () =>{
+    await api
+    .get('/api/blog')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+})
+test('all blogs are returned', async() => {
+    const response = await api.get('api/blog')
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
+afterAll ( async () => {
+    await mongoose.connection.close()
+})
