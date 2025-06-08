a19319a2f40734ae13870416c8cf2dbf4889cb47
ElizabethMM36 <194572580+ElizabethMM36@users.noreply.github.com>
2025-06-08 17:54:20 +0400

    ex 4.17

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -1,23 +1,35 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogRouter.get('/', async (request, response) => {
-  const blog = await Blog.find({})
-  response.json(blog)
+
+blogRouter.get('/', async (req, res) => {
+  try {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    res.json(blogs)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'internal server error' })
+  }
 })
+
 
 blogRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const user = await User.findOne({})
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user._id // Assuming you want to associate the first user with the blog
   })
 
   try {
     const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
     response.status(201).json(savedBlog)
   } catch (error) {
     next(error)  // Important: passes ValidationError to middleware
